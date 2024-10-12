958b1865cb8b8353f848c3c40a2eb897d4777d60
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2024-10-12 21:09:03 +0530

    other commit

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiRequest = axios.create({
-    baseURL: "https://vino-estate-api.vercel.appapi",
+    baseURL: "https://vino-estate-api.vercel.app/api",
     withCredentials:true
 })
 
